faced60c5f55857271ffe94e79e6f034e02c26c4
niamkvn <niamkvn@gmail.com>
2025-03-24 00:14:37 +0700

    update

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 import { logBanner } from "./utils/app.util.ts";
 import Updater from "./services/updater.service.ts";
 
-logBanner()
-const updater = new Updater();
-updater.checkForUpdates();
+logBanner();
+const args = Deno.args;
+
+if (args.includes("--update")) {
+  const updater = new Updater();
+  await updater.checkForUpdates();
+}
+console.log('run')
 // Main.run();
 // logBenner()
